d7cd9d27d4ab1d381904c87941ee75f4df83b602
Alan-H-Wang <54087343+Alan-H-Wang@users.noreply.github.com>
2019-09-13 15:46:53 -0700

    Update HomeViewController.swift
    facebook button code

--- Fitness/HomeViewController.swift
+++ Fitness/HomeViewController.swift
@@ -5,26 +5,22 @@
 //  Created by Albert Stanley on 8/21/19.
 //  Copyright © 2019 Alan Wang. All rights reserved.
 //
-
-import Foundation
 import UIKit
+import FacebookCore
+import FacebookLogin
+import FBSDKCoreKit
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginButton = FBLoginButton(permissions: [.publicProfile])
+        loginButton.center = view.center
+        self.view.addSubview(loginButton)
+        
+  
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+
+
 }
